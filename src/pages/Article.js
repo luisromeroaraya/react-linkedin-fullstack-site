9aca7ecc0f2d238bf5e3ab070247ea7eebe46bdd
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-12-08 19:16:07 +0100

    Added article-content with some articles  to show

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import articleContent from "./article-content.js";
 
 function Article() {
   const { name } = useParams();
+  const article = articleContent.find(article => article.name === name);
+  if (!article) {
+    return <h1>Article does not exist!</h1>;
+  }
+
   return (
     <div>
-      <h1>This is an article about {name}</h1>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
     </div>
   );
 }
