932a2b1573f7a0981f8dc01ce939cc040bcc41ea
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-12-08 18:31:23 +0100

    Add more pages and router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/HomePage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} exact />
+      <Route path="/about" element={<About />} exact />
+      <Route path="/articles" element={<Articles />} exact />
+      <Route path="/article" element={<Article />} exact />
+    </Routes>
   );
 }
 
