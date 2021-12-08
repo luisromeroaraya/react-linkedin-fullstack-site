4a46518199bf17fe64d2dc51022c8f2d42bd8ffa
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-12-08 19:27:14 +0100

    Add Article List in the Articles page

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,9 +1,19 @@
 import React from "react";
+import articleContent from "./article-content.js";
+import { Link } from "react-router-dom";
 
 function Articles() {
   return (
     <div>
       <h1>Articles</h1>
+      {articleContent.map((article,key) => (
+        <div key={key}>
+          <Link className="article-list-item" to={`/articles/${article.name}`}>
+            <h2>{article.title}</h2>
+            <p>{article.content[0].substring(0, 150)}...</p>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 }
