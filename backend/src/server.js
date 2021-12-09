8ae77467b8ca9951fa6ff94d9a356fc5ad667dcc
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-12-09 15:20:22 +0100

    Add add-comment API endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,17 @@ import bodyParser from "body-parser";
 const articlesInfo = {
   "learn-react": {
     upvotes: 0,
+    comments: [],
   },
   "learn-node": {
     upvotes: 0,
+    comments: [],
   },
   "my-thoughts-on-resumes": {
     upvotes: 0,
+    comments: [],
   },
-}
+};
 
 const app = express();
 
@@ -20,7 +23,21 @@ app.use(bodyParser.json());
 app.post("/api/articles/:name/upvote", (req, res) => {
   const articleName = req.params.name;
   articlesInfo[articleName].upvotes += 1;
-  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+  res
+    .status(200)
+    .send(
+      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+    );
+});
+
+app.post("/api/articles/:name/add-comment", (req, res) => {
+  const articleName = req.params.name;
+  const comment = { username: req.body.username, text: req.body.text };
+  articlesInfo[articleName].comments.push(comment);
+  res
+    .status(200)
+    .send(articlesInfo[articleName]
+    );
 });
 
 app.listen(8000, () => {
