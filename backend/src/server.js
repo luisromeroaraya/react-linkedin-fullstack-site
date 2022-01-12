1236d7756e5d0aac98ca343bb946881859152104
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2022-01-12 12:42:40 +0100

    Added get endpoint for showing articles from the DB in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,24 +1,45 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { MongoClient } from "mongodb";
 
-const articlesInfo = {
-  "learn-react": {
-    upvotes: 0,
-    comments: [],
-  },
-  "learn-node": {
-    upvotes: 0,
-    comments: [],
-  },
-  "my-thoughts-on-resumes": {
-    upvotes: 0,
-    comments: [],
-  },
-};
+// we installed mongoDB so we dont need this constant anymore
+// const articlesInfo = {
+//   "learn-react": {
+//     upvotes: 0,
+//     comments: [],
+//   },
+//   "learn-node": {
+//     upvotes: 0,
+//     comments: [],
+//   },
+//   "my-thoughts-on-resumes": {
+//     upvotes: 0,
+//     comments: [],
+//   },
+// };
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+  try {
+    const articleName = req.params.name;
+
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+    const db = client.db('my-blog');
+
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+
+    client.close();
+  } 
+  
+  catch (error) {
+    res.status(500).json({ message: 'Error connecting to the db', error });
+  }
+});
+
 
 app.post("/api/articles/:name/upvote", (req, res) => {
   const articleName = req.params.name;
@@ -34,10 +55,7 @@ app.post("/api/articles/:name/add-comment", (req, res) => {
   const articleName = req.params.name;
   const comment = { username: req.body.username, text: req.body.text };
   articlesInfo[articleName].comments.push(comment);
-  res
-    .status(200)
-    .send(articlesInfo[articleName]
-    );
+  res.status(200).send(articlesInfo[articleName]);
 });
 
 app.listen(8000, () => {
